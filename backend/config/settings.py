a0c5b301ab59bcf389d3a89ee42e4d5ce5d7a9d5
Bishalmsg <ghartimagarbishal87@gmail/com>
2026-09-05 17:27:05 +0545

    wip settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-!dd)c2+=o(zaw&()ntw91cvq6ld%_mhbj-3+(zwx0prw-rt&_c'
 # SECRET_KEY = os.environ.get('SECRET_KEY')
-=======
 # SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
